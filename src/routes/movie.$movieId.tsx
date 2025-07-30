37a6c5bdf59b820d716f8989a9bd1b08354ee670
Biruk-N <biruk.rcd.1912@gmail.com>
2025-07-30 22:06:14 +0300

    fix type issue

--- src/routes/movie.$movieId.tsx
+++ src/routes/movie.$movieId.tsx
@@ -221,7 +221,7 @@ function MovieDetailsPage() {
               <div className="flex flex-wrap gap-4">
                 <a
                   // href={`https://www.imdb.com/title/tt${movie.id.toString().padStart(7, '0')}`}
-                  href={`https://www.imdb.com/title/${movie.imdb_id.toString().padStart(7, '0')}`} 
+                  href={`https://www.imdb.com/title/${(movie.imdb_id ?? "").toString().padStart(7, '0')}`} 
                   target="_blank"
                   rel="noopener noreferrer"
                   className="inline-flex items-center px-6 py-3 bg-blue-600 hover:bg-blue-700 text-white font-semibold rounded-lg transition-colors"
